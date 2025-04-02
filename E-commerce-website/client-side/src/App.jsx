4798ea9169b41e83f7f3c711848efd8015806265
abhilashaahingare <abhilashaahingare.02@gmail.com>
2025-04-02 17:55:00 +0530

    e commerce website

---
+++ E-commerce-website/client-side/src/App.jsx
@@ -0,0 +1,17 @@
+import react from "react";
+import { BrowserRouter, Route, Router } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Router>
+          <Route></Route>
+        </Router>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
