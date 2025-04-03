bc802dd2a7e73fd0f1611d104b58af6b05943eef
abhilashaahingare <abhilashaahingare.02@gmail.com>
2025-04-03 12:32:12 +0530

    header section will be complete

--- E-commerce-website/client-side/src/App.jsx
+++ E-commerce-website/client-side/src/App.jsx
@@ -1,14 +1,15 @@
-import react from "react";
-import { BrowserRouter, Route, Router } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserLayout from "./componets/layouts/UserLayout";
 import "./App.css";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Router>
-          <Route></Route>
-        </Router>
+        <Routes>
+          <Route path="/" element={<UserLayout />}></Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
