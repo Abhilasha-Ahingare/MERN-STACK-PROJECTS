52a68bd098ce4f3f6916c7cafb2538c737a34a15
abhilashaahingare <abhilashaahingare.02@gmail.com>
2025-04-04 13:51:57 +0530

    auth page was complete and also profile page was completed

--- E-commerce-website/client-side/src/App.jsx
+++ E-commerce-website/client-side/src/App.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./componets/layouts/UserLayout";
 import "./App.css";
-import Home from "./store/Home";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Profile from "./Pages/Profile";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<UserLayout />}>
             <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
           </Route>
         </Routes>
       </BrowserRouter>
