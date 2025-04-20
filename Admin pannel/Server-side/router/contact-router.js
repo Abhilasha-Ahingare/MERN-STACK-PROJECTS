4f44a6d43c5b967468d89a9e1db83c95d5e79444
Abhilasha -Ahingare <abhilashaahingare.02@gmail.com>
2025-04-20 19:26:40 +0530

    Delete Admin pannel directory

--- Admin pannel/Server-side/router/contact-router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const contactControllers = require("../Controllers/contact-controller");
-
-router.route("/contact").post(contactControllers);
-
-module.exports = router;
--- Admin pannel/Server-side/router/services-router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const service = require("../Controllers/services-controller");
-
-router.route("/service").get(service);
-
-module.exports = router;
